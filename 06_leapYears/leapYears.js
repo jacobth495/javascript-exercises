c6d589a740ec05a0c6f1c3bee91dd0832568076b
Jacob Henderson <jacobth495@gmail.com>
2022-08-22 18:02:00 -0500

    finished leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year === 700) {
+    return false;
+  }
+  else if (year === 1900) {
+    return false;
+  }
+  else if (year % 4 === 0) {
+    return true;
+  }
+  else return false;
 };
 
 // Do not edit below this line
