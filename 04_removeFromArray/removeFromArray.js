9c56231d29db97588da69a832e0ce42dc89d2693
Jacob Henderson <jacobth495@gmail.com>
2022-08-22 14:57:36 -0500

    finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...items) {
 
+  for (i of items) {
+
+    if (theArray.indexOf(i) === -1) {
+      continue;
+    }
+    else {
+      index = (theArray.indexOf(i))
+      theArray.splice(index, 1)
+    }
+  }
+  
+  return theArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
