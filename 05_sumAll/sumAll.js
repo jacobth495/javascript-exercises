cc8af3b4d3184ce3fa8d958009ba405786e4b7f3
Jacob Henderson <jacobth495@gmail.com>
2022-08-22 16:44:22 -0500

    finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let sum = 0
 
+  type = typeof(end);
+
+  if (type === String) {
+    return 'ERROR'
+  }
+  else {
+
+    if (start > end) {
+      for (i = start; i >= end; i--) {
+        sum += i;
+        }
+      }
+    else {
+      for (i = start; i <= end; i++) {
+        sum += i;
+        }
+      }
+  }
+
+  
+  if (sum > 0) {
+    return sum
+  }
+  else {
+    return 'ERROR'
+  }
+  
+  
 };
 
 // Do not edit below this line
