6cdf067c3d96550531d340e6b412bea52c32591e
Jacob Henderson <jacobth495@gmail.com>
2022-08-23 12:26:11 -0500

    finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  c = (f - 32) * (5/9);
+  cel = c.toFixed(1);
+  return parseFloat(cel)
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  f = (c * (9/5) + 32)
+  fer = f.toFixed(1)
+  return parseFloat(fer)
 };
 
 // Do not edit below this line
