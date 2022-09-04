1bade9e35758a042f7476caea366d087370c3610
Jacob Henderson <jacobth495@gmail.com>
2022-09-04 11:09:21 -0500

    Finished calculator section

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	c = a + b;
+  return c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	c = a - b;
+  return c;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  total = numbers.reduce((previousValue, currentValue) => {
+    return previousValue += currentValue;
+  },0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  total = numbers.reduce((previousValue, currentValue) => {
+    return previousValue *= currentValue;
+  },1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0)
+  {
+     return 1;
+  }
+   return x * factorial(x-1);
 };
 
 // Do not edit below this line
